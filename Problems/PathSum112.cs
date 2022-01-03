308cf97e956472609807fc90a6eb4ace4780b5c1
Ryan Petit <petit.ryank@gmail.com>
2022-01-03 13:39:29 -0500

    added path sum solution

---
+++ Problems/PathSum112.cs
@@ -0,0 +1,23 @@
+﻿using Problems.Helpers;
+
+namespace Problems
+{
+    public class PathSum112
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+
+            if (root == null) return false;
+
+            if (isLeaf(root)) return root.val == targetSum;
+
+            return HasPathSum(root.left, targetSum - root.val)
+                || HasPathSum(root.right, targetSum - root.val);
+        }
+
+        private bool isLeaf(TreeNode root)
+        {
+            return root.left == null && root.right == null;
+        }
+    }
+}
